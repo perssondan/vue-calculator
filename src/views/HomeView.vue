2c7b6beed73cc42ef8ae4d7306738d0107f9db88
Dan Persson <dan.persson@cenito.se>
2022-07-14 22:12:49 +0200

    Add prettier config
    - Added single quote rule

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <CalculatorHeader />
+    <StandardDisplay />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import CalculatorHeader from '@/components/CalculatorHeader.vue';
+import StandardDisplay from '@/components/StandardDisplay.vue';
 
 export default {
-  name: "HomeView",
+  name: 'HomeView',
   components: {
-    HelloWorld,
+    CalculatorHeader,
+    StandardDisplay,
   },
 };
 </script>
